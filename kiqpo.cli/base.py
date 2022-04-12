7288e5e6d44b1ca4ec96676ce34e1e5669f5aa88
shajin-sha <shajin-sha@users.noreply.github.com>
2022-04-12 11:06:09 +0000

    ✅ Ready to clone and code.

--- kiqpo.cli/base.py
+++ kiqpo.cli/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+kiqpo.cli base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the kiqpo.cli project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,6 @@
 import pytest
 
-from project_name import BaseClass, base_function
+from kiqpo.cli import BaseClass, base_function
 
 given = pytest.mark.parametrize
 
